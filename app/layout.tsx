70fe27ee30a201644ff2b53d843fef4615aa97eb
ShivaniRTR <158785388+ShivaniRTR@users.noreply.github.com>
2024-04-12 16:47:37 +0530

    adding meta tag to verify site ownership on google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="Vlve4TcWDhbJrYv2PnDKUqtt4dJhFi1O80AN7wqq9Oc" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
